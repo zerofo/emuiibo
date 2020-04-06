1079f06afb27616d25a465c1910f7e3f99ce59c0
XorTroll <xabofdez@outlook.es>
2020-04-06 15:59:24 +0200

    Fix mii services dumping default miis instead of database ones

--- emuiibo/source/ipc/mii/mii_Service.cpp
+++ emuiibo/source/ipc/mii/mii_Service.cpp
@@ -12,9 +12,9 @@ namespace ipc::mii {
         }
         auto rc = smGetService(&g_mii_srv, "mii:u");
         if(R_SUCCEEDED(rc)) {
-            auto code = SpecialMiiKeyCode::Normal;
+            u32 in = static_cast<u32>(SpecialMiiKeyCode::Normal);
             // GetDatabaseService
-            rc = serviceDispatchIn(&g_mii_srv, 0, code,
+            rc = serviceDispatchIn(&g_mii_srv, 0, in,
                 .out_num_objects = 1,
                 .out_objects = &g_mii_database_srv,
             );
@@ -41,13 +41,13 @@ namespace ipc::mii {
     }
 
     Result GetCount(u32 *out_count) {
-        auto flag = SourceFlag::Default;
-        return serviceDispatchInOut(&g_mii_database_srv, 2, flag, *out_count);
+        auto in = static_cast<u32>(SourceFlag::Database);
+        return serviceDispatchInOut(&g_mii_database_srv, 2, in, *out_count);
     }
 
     static inline Result GetCharInfos(CharInfo *buf, size_t sz, u32 *out_size) {
-        auto flag = SourceFlag::Default;
-        return serviceDispatchInOut(&g_mii_database_srv, 4, flag, *out_size, 
+        auto in = static_cast<u32>(SourceFlag::Database);
+        return serviceDispatchInOut(&g_mii_database_srv, 4, in, *out_size, 
             .buffer_attrs = { SfBufferAttr_HipcMapAlias | SfBufferAttr_Out },
             .buffers = { { buf, sz } },
         );
@@ -68,7 +68,6 @@ namespace ipc::mii {
             auto rc = GetCharInfos(buf, mii_count * sizeof(CharInfo), &out_size);
 
             auto size = std::min(mii_count, out_size);
-
             if(R_SUCCEEDED(rc)) {
                 if(idx < size) {
                     memcpy(out_info, &buf[idx], sizeof(CharInfo));
